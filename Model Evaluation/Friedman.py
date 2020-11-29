d0bf2bc9500fb71cfa917055baf380103d7247d7
Stratos Grigoroudis <61066632+StratosGrig@users.noreply.github.com>
2020-11-29 09:12:08 +0200

    Project 6

--- Model Evaluation/Friedman.py
+++ Model Evaluation/Friedman.py
@@ -1,16 +1,19 @@
 import pandas as pd
 from scipy.stats import friedmanchisquare
+import numpy as np
 
 # enter data
 data = pd.read_csv(r'D:\Msc\ML\Projects\Εργασία 6η\algo_performance.csv')
-print(data)
+
 # calculate friedman Test
-statists, pValue = friedmanchisquare(data['C4.5'], data['1-NN'], data['NaiveBayes'], data['Kernel'], data['CN2'])
-print((statists, pValue))
+statistics, pValue = friedmanchisquare(data['C4.5'], data['1-NN'], data['NaiveBayes'], data['Kernel'], data['CN2'])
+print('Statistics = %.3f, p-Value = %s' % (statistics, pValue))
 
 # Set alpha value
-alpha = 0.05
-if pValue > alpha:
-    print('Fail to reject Hypothesis')
-else:
-    print('Reject Hypothesis')
+for alpha in np.arange(0, 0.05, 0.0001):
+    if pValue > alpha:
+        print(
+            "For alpha = {0} --> Fail to reject Hypothesis ( They don't have statistically significant difference)".format(
+                alpha))
+    else:
+        print("For alpha = {0} --> Reject Hypothesis ( They have statistically significant difference)".format(alpha))
